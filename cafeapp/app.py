5459a601ee1d352b8abd6a85595f25c1165eba9a
‘Evan <puzonevan@gmail.com>
2020-08-12 15:31:32 -0700

    app routes in own file, re organized, and clarifying comments

--- cafeapp/app.py
+++ cafeapp/app.py
@@ -1,101 +1,22 @@
-from flask import Flask, render_template, request
+# Flask Imports 
+from flask import Flask
+# Form Imports 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from flask_sqlalchemy import SQLAlchemy 
 from wtforms.validators import DataRequired 
-import os, sorts
+# SQL Imports 
+from flask_sqlalchemy import SQLAlchemy 
 
 
+""" Initialize / configure app """
 app = Flask(__name__, static_url_path='/static')
 
 app.config['SECRET_KEY'] = 'MY_SECRET_KEY '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db' 
 
+""" Bind app to SQL """
 db = SQLAlchemy(app)
-
-@app.route('/', methods=["GET", "POST"])
-@app.route('/home', methods=["GET", "POST"])
-def home(): 
-    return render_template("home.html")
-
-@app.route('/gallery')
-def gallery(): 
-
-    cafeimages = sorts.mergeSort(os.listdir('./static/CafeImgs'))
-    coffeeimages = sorts.mergeSort(os.listdir('./static/CoffImgs'))
-    cakeimages = sorts.mergeSort(os.listdir('./static/CakeImgs'))
-
-    return render_template("gallery.html", 
-        cafeimages=cafeimages, 
-        cakeimages=cakeimages, 
-        coffeeimages=coffeeimages, 
-    )
-
-@app.route('/menu')
-def menu(): 
-    return render_template("menus.html")
-
-@app.route('/menu/<int:menu_id>')
-def singlemenu(menu_id):
-    return render_template("menu.html", id=menu_id)
-
-@app.route('/locations')
-def location(): 
-    
-    locations = [
-        {
-            'city': 'San Francisco',
-            'address': '201 Fell St, CA 94102', 
-            'number': '+14154372734',
-            'image': 'static/LocImgs/sanfran1.jpg'
-        }, 
-        {
-            'city': 'Seoul',
-            'address': '10 Hongik-ro, Seogyo-dong, Mapo-gu, Seoul', 
-            'number': '+82 2-332-7470',
-            'image': 'static/LocImgs/korea1.jpg',
-        }, 
-        {
-            'city': 'Tokyo',
-            'address': '〒 103-0027 Tokyo, Chuo City, Nihonbashi, 2 Chome−11−2 日', 
-            'number': '+81 3-6262-3439',
-            'image': 'static/LocImgs/tokyo1.jpg'
-        }, 
-    ]
-
-    return render_template("locations.html",
-        locations=locations, 
-    )
-
-@app.route('/contact', methods=["GET", "POST"])
-def contact(): 
-
-    contactform = ContactForm()
-    applicationform = ApplicationForm()
-    reviewform = ReviewForm()
-
-    if contactform.validate_on_submit(): 
-        print(contactform.name.data)
-        print(contactform.email.data)
-        print(contactform.subject.data)
-        print(contactform.message.data)
-
-    if applicationform.validate_on_submit(): 
-        print(applicationform.name.data)
-        print(applicationform.number.data)
-        print(applicationform.description.data)
-
-    if reviewform.validate_on_submit(): 
-        print(reviewform.name.data)
-        print(reviewform.review.data)
-
-
-    return render_template("contact.html", 
-        contactform=contactform,
-        applicationform=applicationform,
-        reviewform=reviewform,
-    )
 
 
 """ Form Classes """ 
@@ -117,4 +38,24 @@ class ApplicationForm(FlaskForm):
     number = StringField("Number", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+""" Database Models """ 
+class Contact(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), unique = True, index = True)
+    email = db.Column(db.String(100), unique = True, index = True)
+    subject = db.Column(db.String(50), unique = False, index = False)
+    message = db.Column(db.String(300), unique = False, index = False)
 
+class Review(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), unique = True, index = True)
+    review = db.Column(db.String(1000), unique = True, index = False)
+
+class Application(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), unique = True, index = True)
+    number = db.Column(db.String(15), unique = True, index = False)
+    description = db.Column(db.String(1000), unique = True, index = False)
+
+
+import routes 
---
+++ cafeapp/routes.py
@@ -0,0 +1,116 @@
+from app import app, db
+from flask import Flask, render_template, request
+from app import ContactForm, ReviewForm, ApplicationForm
+from app import Contact, Review, Application
+import os, sorts
+
+""" Home Route """
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
+def home(): 
+    return render_template("home.html")
+
+
+""" Gallery Route """
+@app.route('/gallery')
+def gallery(): 
+
+    cafeimages = sorts.mergeSort(os.listdir('./static/CafeImgs'))
+    coffeeimages = sorts.mergeSort(os.listdir('./static/CoffImgs'))
+    cakeimages = sorts.mergeSort(os.listdir('./static/CakeImgs'))
+
+    return render_template("gallery.html", 
+        cafeimages=cafeimages, 
+        cakeimages=cakeimages, 
+        coffeeimages=coffeeimages, 
+    )
+
+""" Menus Route """
+@app.route('/menu')
+def menu(): 
+    return render_template("menus.html")
+
+""" Single Menu Route """
+@app.route('/menu/<int:menu_id>')
+def singlemenu(menu_id):
+    return render_template("menu.html", id=menu_id)
+
+""" Locations Route """
+@app.route('/locations')
+def location(): 
+    
+    locations = [
+        {
+            'city': 'San Francisco',
+            'address': '201 Fell St, CA 94102', 
+            'number': '+14154372734',
+            'image': 'static/LocImgs/sanfran1.jpg'
+        }, 
+        {
+            'city': 'Seoul',
+            'address': '10 Hongik-ro, Seogyo-dong, Mapo-gu, Seoul', 
+            'number': '+82 2-332-7470',
+            'image': 'static/LocImgs/korea1.jpg',
+        }, 
+        {
+            'city': 'Tokyo',
+            'address': '〒 103-0027 Tokyo, Chuo City, Nihonbashi, 2 Chome−11−2 日', 
+            'number': '+81 3-6262-3439',
+            'image': 'static/LocImgs/tokyo1.jpg'
+        }, 
+    ]
+
+    return render_template("locations.html",
+        locations=locations, 
+    )
+
+""" Contact Route """
+@app.route('/contact', methods=["GET", "POST"])
+def contact(): 
+
+    contactform = ContactForm()
+    applicationform = ApplicationForm()
+    reviewform = ReviewForm()
+
+    if contactform.validate_on_submit(): 
+        newcontact = Contact(
+            name = contactform.name.data, 
+            email = contactform.email.data, 
+            subject = contactform.subject.data, 
+            message = contactform.message.data
+        )
+        db.session.add(newcontact)
+        try: 
+            db.session.commit()
+        except: 
+            db.session.rollback()
+
+    if applicationform.validate_on_submit(): 
+        newapp = Application(
+            name = applicationform.name.data, 
+            number = applicationform.number.data,
+            description = applicationform.description.data
+        )
+        db.session.add(newapp)
+        try: 
+            db.session.commit()
+        except: 
+            db.session.rollback()
+
+    if reviewform.validate_on_submit(): 
+        newreview = Review(
+            name = reviewform.name.data, 
+            review = reviewform.review.data
+        )
+        db.session.add(newreview)
+        try: 
+            db.session.commit()
+        except: 
+            db.session.rollback()
+
+
+    return render_template("contact.html", 
+        contactform=contactform,
+        applicationform=applicationform,
+        reviewform=reviewform,
+    )
